4b40e222c4212af26fc1dcab8308c354a9a390a3
Kim <kim.jamie.bradley@gmail.com>
2017-11-11 13:38:18 +0000

    Update download_stripped_hosts.py

--- download_stripped_hosts.py
+++ download_stripped_hosts.py
@@ -1,124 +1,190 @@
-import urllib.request
+import copy, urllib.request, tldextract
 
 file_name = "hosts.txt"
-whitelist = dict()
+whitelist = list()
 
 def addToDict(d, data):
-    for line in data.split('\n'):
-        lineStr = ""
-        lineStr += line
-        # Skips unusable lines
-        if not lineStr or lineStr.startswith('\n'):
-            continue
-        if lineStr.startswith('127.0.0.1'):
-            continue
-        if lineStr.startswith('255.255.255.255'):
-            continue
-        if lineStr.startswith('.'):
-        	continue
-        if '!' in lineStr:
-            continue
-        if ':' in lineStr:
-            continue
-        if '@' in lineStr:
-            continue 
-        if '$' in lineStr:
-            continue
-        if '#' in lineStr:
-            continue
-        if '*' in lineStr:
-            continue
-        if '/' in lineStr:
-            continue
-        if '%' in lineStr:
-            continue
-        if '=' in lineStr:
-        	continue
-       	if '?' in lineStr:
-       		continue
+	# Returns a sorted, stripped dictionary of hosts, with the registered domain as the key
+	print('Adding hosts to dictionary')
+	for line in data.split('\n'):
+		lineStr = ""
+		lineStr += line
+		# Skip unusable lines
+		if not lineStr:
+			continue
+		if lineStr.startswith('127.0.0.1'):
+			continue
+		if lineStr.startswith('255.255.255.255'):
+			continue
+		if '!' in lineStr:
+			continue
+		if ':' in lineStr:
+			continue
+		if '@' in lineStr:
+			continue
+		if '#' in lineStr:
+			continue
+		if '$' in lineStr:
+			continue
+		if '*' in lineStr:
+			continue
+		if '?' in lineStr:
+			continue
+		if '=' in lineStr:
+			continue
         # Strips any whitespace
-        lineStr = lineStr.strip()
+		lineStr = lineStr.strip()
         # Strips initial pipe symbols
-        lineStr = lineStr.replace('||', '')
+		lineStr = lineStr.replace('||', '')
         # Strips use of 'www.'
-        lineStr = lineStr.replace('www.', '')
+		lineStr = lineStr.replace('www.', '')
         # Strips initial '0.0.0.0 ' from host files
-        lineStr = lineStr.replace('0.0.0.0 ', '')
+		lineStr = lineStr.replace('0.0.0.0 ', '')
         # Strips use of '^third-party'
-        lineStr = lineStr.replace('^third-party', '')
+		lineStr = lineStr.replace('^third-party', '')
         # Strips extra '^'
-        lineStr = lineStr.replace('^', '')
-        # Checks values against whitelist
-        is_safe = False
-        for key in whitelist:
-        	if lineStr == key:
-        		is_safe = True
-        if is_safe:
-        	continue
+		lineStr = lineStr.replace('^', '')
+        # Removes unusable information after first '/'
+		if '/' in lineStr:
+			lineStr = lineStr.split('/')[0]
+        # Checks values against whitelist and skips safe hosts
+		is_safe = False
+		for key in whitelist:
+			if lineStr == key:
+				is_safe = True
+		if is_safe:
+			continue
         # Skips final unusables
-        if '^' in lineStr:
-            continue
-        if lineStr.endswith('.'):
-        	continue
-        if lineStr.endswith('.js'):
-        	continue
+		if lineStr.startswith('.') or lineStr.endswith('.') or lineStr.endswith('.js'):
+			continue
+		# Adds the host to a dictionary which serves as the value to a parent dictionary (passed in the method argument), with the registered domain as the key
+		ext = tldextract.extract(lineStr)
+		base_domain = ext.registered_domain
+		td = d.get(base_domain, dict())
+		td[lineStr] = td.get(lineStr, 0) + 1
+		d[base_domain] = td
+	return d
 
-        d[lineStr] = d.get(lineStr, 0) + 1
-
-    return d
+def longestStringInList(l):
+	index = 0
+	for i in range(0, len(l)):
+		if l[i] > l[index]:
+			index = i
+	return l[index]
 
 def downloadHosts(url_str, d):
-    response = urllib.request.urlopen(url_str)
-    data = response.read()
-    text = data.decode('utf-8')
-    return addToDict(d, text)
+	# Downloads hosts and passes them to the method addToDict() which returns a sorted, stripped dict of hosts
+	print('Downloading hosts from ' + url_str)
+	response = urllib.request.urlopen(url_str)
+	data = response.read()
+	text = data.decode('utf-8')
+	return addToDict(d, text)
 
 def downloadBetterFYITrackerList(d):
-    url = "https://raw.githubusercontent.com/anarki999/Adblock-List-Archive/master/Better.fyiTrackersBlocklist.txt"
-    return downloadHosts(url, d)
+	url = "https://raw.githubusercontent.com/anarki999/Adblock-List-Archive/master/Better.fyiTrackersBlocklist.txt"
+	return downloadHosts(url, d)
 
 def downloadAdawayHosts(d):
-    url = "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt"
-    return downloadHosts(url, d)
+	url = "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt"
+	return downloadHosts(url, d)
 
 def downloadYoYoHosts(d):
-    url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0"
-    return downloadHosts(url, d)
+	url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0"
+	return downloadHosts(url, d)
 
 def downloadAntiMicrosoftHosts(d):
-    url = "https://raw.githubusercontent.com/tyzbit/hosts/master/data/tyzbit/hosts"
-    return downloadHosts(url, d)
+	url = "https://raw.githubusercontent.com/tyzbit/hosts/master/data/tyzbit/hosts"
+	return downloadHosts(url, d)
 
 def downloadAdguardSpywareHosts(d):
-    url = "https://filters.adtidy.org/extension/chromium/filters/3.txt"
-    return downloadHosts(url, d)
+	url = "https://filters.adtidy.org/extension/chromium/filters/3.txt"
+	return downloadHosts(url, d)
 
 def downloadPersonalHosts(d):
-    url = "https://raw.githubusercontent.com/grufwub/DNS-Blocklist-Compiler/master/hosts.txt"
-    return downloadHosts(url, d)
+	url = "https://raw.githubusercontent.com/grufwub/DNS-Blocklist-Compiler/master/hosts.txt"
+	return downloadHosts(url, d)
 
 def getWhitelist():
-	url = "https://raw.githubusercontent.com/grufwub/DNS-Blocklist-Compiler/master/whitelist.txt"
-	downloadHosts(url, whitelist)
+	# Downloads from my manually compiled whitelist on Github to prevent sites from being blacklisted
+	url_str = "https://raw.githubusercontent.com/grufwub/DNS-Blocklist-Compiler/master/whitelist.txt"
+	print('Downloading hosts from ' + url_str)
+	response = urllib.request.urlopen(url_str)
+	data = response.read()
+	text = data.decode('utf-8')
+	for line in text:
+		if not line or line.startswith('/') or line.startswith('\n'):
+			continue
+		whitelist.append(line)
 
+def getOrderedKeyList(d):
+	l = list()
+	for i in range(0, 10):
+		if d.get(i):
+			l.append(i)
+	return l
+
+def getHostLengthDictionary(d):
+	return_dict = dict()
+	for key in d.keys():
+		s = key.split('.')
+		i = len(s)
+		return_dict.setdefault(i, list()).append(key)
+	return return_dict
+
+def getUniqueHosts(d):
+	# Returns a list of unique hosts for each registered domain, that don't overlap subdomains (e.g. stats.facebook.com and s.stats.facebook.com), keeping only the shortest.
+	# For each registered domain, goes through the listed hosts and creates a dictionary with {"Number of domain levels" : List[domain, domain, domain]}
+	length_dict = getHostLengthDictionary(d)
+	return_list = list()
+	# Creates an ordered list of keys (which are the domain level ints).
+	key_list = getOrderedKeyList(length_dict)
+	min_length = min(key_list)
+	min_length_host = length_dict[min_length][0]
+	ext = tldextract.extract(min_length_host)
+	# If dictionary contains the registered domain (so all traffic should be blocked), returns only this.
+	if min_length_host == ext.registered_domain:
+		return_list.append(min_length_host)
+	else:
+		# Else goes through the dictionary and finds unique non-overlapping domains
+		previous = list()
+		# BUG: this for loop seems to create duplicates in some instances
+		for length in key_list:
+			current = length_dict[length]
+			if length == min_length:
+				previous = current
+				return_list.extend(current)
+			else:
+				add_to_return_list = list()
+				for host in return_list:
+					for entry in current:
+						if host not in entry and len(entry) > len(host):
+							add_to_return_list.append(entry)
+				return_list.extend(add_to_return_list)
+	return return_list
+			
 def main():
-    d = dict()
-    getWhitelist()
-    downloadPersonalHosts(d)
-    downloadBetterFYITrackerList(d)
-    downloadAdawayHosts(d)
-    downloadYoYoHosts(d)
-    downloadAntiMicrosoftHosts(d)
-    downloadAdguardSpywareHosts(d)
+	d = dict()
+	getWhitelist()
+	downloadPersonalHosts(d)
+	downloadBetterFYITrackerList(d)
+	downloadAdawayHosts(d)
+	downloadYoYoHosts(d)
+	downloadAntiMicrosoftHosts(d)
+	downloadAdguardSpywareHosts(d)
 
-    f = open(file_name, 'w')
-    for key in d.keys():
-    	key = key.strip()
-    	f.write(key + '\n')
-    	# print("Added " + key + "\n")
-
-    f.close()
-    print(len(d.keys()))
+	print('Writing hosts to file:\n')
+	f = open(file_name, 'w')
+	count = 0
+	for key in d.keys():
+		host_list = getUniqueHosts(d[key])
+		host_list = list(dict.fromkeys(host_list)) # Hacky fix to remove duplicates since getUniqueHosts() method seems to introduce duplicates in some cases
+		for host in host_list:
+			f.write(host.strip() + '\n')
+			count += 1
+			print(count)
+			print(host)
+		print('------------------------------------')
+	f.close()
 
 if __name__ == '__main__':
-    main()
+	main()
