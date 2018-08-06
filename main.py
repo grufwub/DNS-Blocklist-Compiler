5d7e42b7ff72ec517b2a9557cb5cd213de5165a7
grufwub <kim.jamie.bradley@gmail.com>
2018-08-06 16:43:30 +0100

    Remembered to include new hosts file location in this commit :P

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ def draw_main_menu(scrn):
 	scrn.addstr('1) An option might go here')
 	# do some more stuff
 	scrn.refresh()
+	
+def move_cursor_up(scrn):
+	# stuff
+
+def move_cursor_down(scrn):
+	# stuff
 
 def main(std_scrn):
 	# Clear the screen initially
 	std_scrn.clear()
+	std_scrn.refresh()
 	
 	# Setup curses interface
 	curses.noecho()
@@ -17,7 +24,10 @@ def main(std_scrn):
 	std_scrn.keypad(True)
 	
 	draw_main_menu(std_scrn)
-	std_scrn.getch()
+	
+	# Start accepting input loop!
+	while True:
+		
 
 if __name__ == '__main__':
 	curses.wrapper(main)
