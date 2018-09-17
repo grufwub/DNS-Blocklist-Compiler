32744fd8363b09c29efd13b2e36d001c7edae483
grufwub <kim.jamie.bradley@gmail.com>
2018-09-17 21:54:30 +0100

    Neatened up code a lot. Refactored some into separate file + introduced class based curses-menu instance

--- main.py
+++ main.py
@@ -1,127 +1,54 @@
 import curses
+import ui
 import host_compiler as hc
 import source_handler as sh
 import profile_handler as ph
 
-PROFILE_SELECTED = ""
-ITEM_SELECTED = -1
-MAX_ID = -1
-MENU_ITEMS = None
+profiles = None
+profile_selected = ""
 
-# TODO: in the future make 'running instance' class that you just hand list of menuitems, and header-drawer to ?
 # TODO: alert box if no profile selected
 # TODO: fix weird use case where compiling prints output to screen, then jumps cursor back to beginning of output on quit
-# TODO: move some function defs to different files?
-# TODO: left arrow key goes 'back'
-
-### UI Helpers
-class MenuItem:
-    """
-    Very simple class for now. But easier to add functionalities in the future
-    """
-    def __init__(self, text = "", funct = None):
-        self.text = text
-        self.funct = funct
-
-def main_menuitems():
-    return [
-        MenuItem(text = "Compile blocklist", funct = compile_blocklist),
-        MenuItem(text = "Select profile", funct = profiles_loop),
-        MenuItem(text = "Create / edit profile(s)", funct = edit_profiles_loop),
-        MenuItem(text = "View / edit sources", funct = sources_loop),
-        MenuItem(text = "Cleanup files", funct = clean_files),
-        MenuItem(text = "Help", funct = help_funct),
-        MenuItem(text = "Exit", funct = exit),
-    ]
+# TODO: fix inconsistent use of underscores, especially with term 'menuitems'
 
 def gen_sources_menuitems(sources):
     pass
 
-def gen_profiles_menuitems(profiles):
+def profile_menuitems():
+    profiles = ph.load_profiles()
     menuitems = list()
-    for item in profiles.keys():
-        new_menuitem = MenuItem(item)
+    for name in profiles.keys():
+        new_menuitem = ui.MenuItem(text = name, function = set_profile)
         menuitems.append(new_menuitem)
     return menuitems
-        
 
-def inc_selected_id():
-    global ITEM_SELECTED
-    
-    if ITEM_SELECTED == MAX_ID:
-        ITEM_SELECTED = 0
-    else:
-        ITEM_SELECTED += 1
-    
-def dec_selected_id():
-    global ITEM_SELECTED
-    
-    if ITEM_SELECTED == 0:
-        ITEM_SELECTED = MAX_ID
-    else:
-        ITEM_SELECTED -= 1
+def set_profile(instance):
+    index = instance.item_selected
+    profile_selected = profiles[index]
+    main_loop(instance)
 
-### Handle key inputs
-def handle_main_input(scrn):
-    global ITEM_SELECTED
-    key_pressed = scrn.getkey()
+### MenuItem functions
+def compile_blocklist(instance):
+    instance.close()
     
-    # Handles initial case of screen just created with no menu input
-    if ITEM_SELECTED == -1:
-        if key_pressed == "KEY_UP":
-            ITEM_SELECTED = MAX_ID
-        elif key_pressed == "KEY_DOWN":
-            ITEM_SELECTED = 0
-        return False
-    
-    if key_pressed == "KEY_UP":
-        dec_selected_id()
-        return False
-    elif key_pressed == "KEY_DOWN":
-        inc_selected_id()
-        return False
-    
-    elif key_pressed == "KEY_RIGHT":
-        MENU_ITEMS[ITEM_SELECTED].funct(scrn)
-        return True
-
-def handle_profiles_input(scrn):
-    global ITEM_SELECTED
-    global PROFILE_SELECTED
-    key_pressed = scrn.getkey()
-    
-    # Handles initial case of screen just created with no menu input
-    if ITEM_SELECTED == -1:
-        if key_pressed == "KEY_UP":
-            ITEM_SELECTED = MAX_ID
-        elif key_pressed == "KEY_DOWN":
-            ITEM_SELECTED = 0
-        return False
-    
-    if key_pressed == "KEY_UP":
-        dec_selected_id()
-        return False
-    elif key_pressed == "KEY_DOWN":
-        inc_selected_id()
-        return False
-    elif key_pressed == "KEY_RIGHT":
-        PROFILE_SELECTED = MENU_ITEMS[ITEM_SELECTED].text
-        return True
-
-### Main menuitem functions
-def compile_blocklist(scrn):
-    curses.nocbreak()
-    scrn.keypad(False)
-    curses.echo()
-    curses.endwin()
-    
-    all_profiles = ph.load_profiles()
-    profile = all_profiles[PROFILE_SELECTED]
+    # TODO: check we won't run into an issue here where 'profiles' isn't up to date but 'profile_selected' is name of new profile
+    profile = profiles[profile_selected]
     bl = get_blacklist_sources(profile)
     wl = get_whitelist_sources(profile)
     hc.run(bl, wl)
     print("\nFinished!")
     quit()
+
+def view_profiles(instance):
+    instance.set_menu_items(profile_menuitems())
+    instance.set_header("Profile selection")
+    instance.run_loop()
+
+def edit_profiles(instance):
+    pass
+
+def edit_sources(instance):
+    pass
 
 def clean_files(scrn):
     pass
@@ -129,13 +56,21 @@ def clean_files(scrn):
 def help_funct(scrn):
     pass
 
-def exit(scrn):
-    # Close curses interface
-    curses.nocbreak()
-    scrn.keypad(False)
-    curses.echo()
-    curses.endwin()
+def exit(instance):
+    instance.close()
     quit()
+
+def name_editor(scrn):
+    pass
+
+def source_selector(scrn):
+    pass
+
+def profile_creator(scrn):
+    pass
+
+def profile_editor(scrn):
+    pass
 
 ### Data Handling
 def get_blacklist_sources(profile):
@@ -158,167 +93,31 @@ def get_whitelist_sources(profile):
 			wl.append(source)
 	return wl
 
-### Main UI Drawing
-def draw_menuitems(scrn):
-	global MENU_ITEMS
-	global ITEM_SELECTED
-    
-	# Draw menu items
-	index = 0
-	for item in MENU_ITEMS:
-		if index == ITEM_SELECTED:
-			scrn.addstr("> " + item.text + "\n", curses.A_REVERSE)
-		else:
-			scrn.addstr("> " + item.text + "\n")
-		index += 1
-        
-def draw_profiles_header(scrn):
-    scrn.addstr("----------< Profile Selection Screen >----------\n", curses.A_BOLD)
-    scrn.addstr("\n")
-
-def profiles_loop(scrn):
-    global MENU_ITEMS
-    global MAX_ID
-    global ITEM_SELECTED
-    
-    ITEM_SELECTED = -1
-    MENU_ITEMS = gen_profiles_menuitems( ph.load_profiles() )
-    MAX_ID = len (MENU_ITEMS) - 1
-    
-    # Initial screen draw
-    scrn.clear()
-    draw_profiles_header(scrn)
-    draw_menuitems(scrn)
-    scrn.refresh()
-    
-    while True:
-        # Handle input
-        result = handle_profiles_input(scrn)
-        scrn.clear()
-        if result: break
-    
-        draw_profiles_header(scrn)
-        draw_menuitems(scrn)
-        scrn.refresh()
-
-def draw_edit_profiles_header(scrn):
-    pass
-
-def edit_profiles_loop(scrn):
-    global MENU_ITEMS
-    global MAX_ID
-    
-    MENU_ITEMS = gen_profiles_menuitems(profiles)
-    MAX_ID = len(MENU_ITEMS) - 1
-    
-    # Initial screen draw
-    scrn.clear()
-    draw_sources_header(scrn)
-    draw_menuitems(scrn)
-    scrn.refresh()
-    
-    while True:
-        # Handle input
-        handle_key_input(scrn)
-        
-        scrn.clear()
-        draw_profiles_header(scrn)
-        draw_menuitems(scrn)
-        scrn.refresh()
-
-def draw_sources_header(scrn):
-    pass
-
-def sources_loop(scrn):
-    global MENU_ITEMS
-    global MAX_ID
-    
-    MENU_ITEMS = gen_sources_menuitems(sources)
-    MAX_ID = len(MENU_ITEMS) - 1
-    
-    # Initial screen draw
-    scrn.clear()
-    draw_sources_header(scrn)
-    draw_menuitems(scrn)
-    scrn.refresh()
-    
-    while True:
-        # Handle input
-        handle_key_input(scrn)
-
-        scrn.clear()
-        draw_sources_header(scrn)
-        draw_menuitems(scrn)
-        scrn.refresh()
-
-def draw_main_header(scrn):
-    global PROFILE_SELECTED
-    
-    scrn.addstr("----------< DNS Blocklist Compiler by @grufwub >----------\n", curses.A_BOLD)
-    scrn.addstr("\n")
-    scrn.addstr("Profile selected: [%s]\n" % PROFILE_SELECTED)
-    scrn.addstr("\n")
-
-def main_loop(scrn):
-	global MENU_ITEMS
-	global MAX_ID
-	global ITEM_SELECTED
-    
-	MENU_ITEMS = main_menuitems()
-	MAX_ID = len(MENU_ITEMS) - 1
-	ITEM_SELECTED = -1
-    
-	# Initial screen draw
-	scrn.clear()
-	draw_main_header(scrn)
-	draw_menuitems(scrn)
-	scrn.refresh()
-    
-	# Main menu loop!
-	while True:        
-		# Handle input
-		result = handle_main_input(scrn)
-		if result: break
-        
-		# Draw main menu
-		scrn.clear()
-		draw_main_header(scrn)
-		draw_menuitems(scrn)
-		scrn.refresh()
-
-	# Reaaaaaaally hacky way of doing this. TODO: improve when neatening code in future
-	main_loop(scrn)
+### Menu functions
+def loop_main(instance):
+    instance.set_header("DNS Blocklist Compiler\nBy @grufwub")
+    instance.set_menu_items([
+        ui.MenuItem(text = "Compile blocklist", function = compile_blocklist),
+        ui.MenuItem(text = "Select profile", function = view_profiles),
+        ui.MenuItem(text = "Create / edit profile(s)", function = edit_profiles),
+        ui.MenuItem(text = "View / edit sources", function = edit_sources),
+        ui.MenuItem(text = "Cleanup files", function = clean_files),
+        ui.MenuItem(text = "Help", function = help_funct),
+        ui.MenuItem(text = "Exit", function = exit),
+    ])
+    instance.run_loop()
 
 ### Main 'run' sequence
 def run():
-	# Set initially. TODO: is this the best place to put this?
-	global PROFILE_SELECTED
-	PROFILE_SELECTED = ""
-
 	e = None
 	try:
-		std_scrn = curses.initscr()
-    
-    	# Clear the screen initially
-		std_scrn.clear()
-		std_scrn.refresh()
-
-		# Setup curses interface
-		curses.noecho()
-		curses.cbreak()
-		curses.curs_set(0)
-		std_scrn.keypad(True)
-	
-		# Draw and start accepting input!
-		main_loop(std_scrn)
-#	except Exception as exc:
-#		e = exc
+		instance = ui.MenuInstance()
+		instance.init()
+		loop_main(instance)
+	except Exception as exc:
+		e = exc
 	finally:
-		# Close curses interface
-		curses.nocbreak()
-		std_scrn.keypad(False)
-		curses.echo()
-		curses.endwin()
+		instance.close()
 		if e:
 			print("[!!] Closed with exception: < %s >" % e)
 
---
+++ ui.py
@@ -0,0 +1,185 @@
+import curses
+
+class MenuItem:
+    """
+    MenuItem class
+    
+    self.function MUST accept MenuInstance object as argument)
+    """
+    
+    def __init__(self, text = "", function = None):
+        self.text = text
+        self.function = function
+
+class MenuInstance:
+    """
+    MenuInstance class.
+    Handles running curses interface, input handling etc
+    """
+    
+    def __init__(self):
+        """
+        Initialization
+        """
+        self.screen = curses.initscr()
+        
+        self.header_message = None
+        self.previous_header_message = None # TODO: turn this into a stack to handle >2 nested menus
+        self.next_header_message = None
+        
+        self.menu_items = None
+        self.previous_menu_items = None # TODO: turn this into a stack to handle >2 nested menus
+        self.next_menu_items = None
+        
+        self.item_selected = 0
+        self.previous_selected = -1
+        
+        self.next_function = None
+    
+    def init(self):
+        """
+        Start the curses session
+        """
+        
+        self.screen.clear()
+        self.screen.refresh()
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self.screen.keypad(True)
+    
+    def close(self):
+        """
+        Close the curses session
+        """
+        
+        self.screen.clear()
+        self.screen.refresh()
+        self.screen.keypad(False)
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+    
+    def set_menu_items(self, menu_items = list()):
+        """
+        Set menu items to be used for drawing / function calls on next run loop
+        """
+        
+        if self.menu_items:
+            self.previous_menu_items = self.menu_items
+        self.next_menu_items = menu_items
+    
+    def set_header(self, header_message = ""):
+        """
+        Set header message.
+        Caches previous header message and sets next header ready under next_header_message
+        """
+        
+        if self.header_message:
+            self.previous_header_message = self.header_message
+        self.next_header_message = header_message
+    
+    def run_loop(self):
+        """
+        Main run loop
+        """
+        
+        # Handles case where we come back to menu from another screen
+        if self.previous_selected != -1:
+            self.item_selected = self.previous_selected
+            self.previous_selected = -1 # resets
+        
+        # Grab next set of menu_items to use
+        self.menu_items = self.next_menu_items
+        self.next_menu_items = None
+        
+        # Grab header message to use
+        self.header_message = self.next_header_message
+        self.next_header_message = None
+        
+        # Initial menu draw
+        self.screen.clear()
+        self._draw_header()
+        self._draw_menu()
+        self.screen.refresh()
+        
+        # Main loop
+        while True:
+            result = self._handle_input()
+            if result: break
+            
+            self.screen.clear()
+            self._draw_header()
+            self._draw_menu()
+            self.screen.refresh()
+            
+        # Call function set by input handler
+        # TODO: improve, hacky way of handling this...
+        if self.next_function == self.run_loop:
+            self.run_loop()
+        else:
+            self.next_function(self)
+    
+    def _handle_input(self):
+        """
+        Handle input from run loop. Loop stalls until getkey() returns a value.
+        Only returns true if item selected, or 'back' button pressed
+        """
+        
+        key = self.screen.getkey()
+  
+        if key == "KEY_UP":
+            # Move up the list
+            self._dec_selected_item()
+        elif key == "KEY_DOWN":
+            # Move down the list
+            self._inc_selected_item()
+        elif key == "KEY_LEFT":
+            if self.previous_menu_items == None:
+                return False
+            
+            self.next_menu_items = self.previous_menu_items
+            self.previous_menu_items = None
+            
+            self.next_header_message = self.previous_header_message
+            self.previous_header_message = None
+            
+            self.next_function = self.run_loop
+            return True
+        elif key == "KEY_RIGHT":
+            self.previous_selected = self.item_selected
+            self.next_function = self.menu_items[self.item_selected].function
+            self.item_selected = 0
+            return True
+        
+        return False
+    
+    def _inc_selected_item(self):
+        if self.item_selected == len(self.menu_items) - 1:
+            self.item_selected = 0
+        else:
+            self.item_selected += 1
+    
+    def _dec_selected_item(self):
+        if self.item_selected == 0:
+            self.item_selected = len(self.menu_items) - 1
+        else:
+            self.item_selected -= 1
+    
+    def _draw_header(self):
+        for line in self.header_message.split("\n"):
+            text = ">>>\t" + line + "\n"
+            self.screen.addstr(text, curses.A_BOLD)
+        self.screen.addstr("\n")
+    
+    def _draw_menu(self):
+        index = 0
+        for item in self.menu_items:
+            text = "> " + item.text + "\n"
+            if index == self.item_selected:
+                self.screen.addstr(text, curses.A_REVERSE)
+            else:
+                self.screen.addstr(text)
+            index += 1
+        
+            
