16caf22427b7ccc01d3aaa75902e40a5b5022a49
grufwub <kim.jamie.bradley@gmail.com>
2018-09-30 20:19:07 +0100

    Nearly finished the UI! One or two bugs left and need to add ability to edit sources

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import ui
 import host_compiler as hc
 import source_handler as sh
@@ -22,42 +23,44 @@ def main_menuitems():
         ui.MenuItem(text = "Compile blocklist", function = compile_blocklist),
         ui.MenuItem(text = "Select profile", function = view_profiles),
         ui.MenuItem(text = "Create / edit profile(s)", function = edit_profiles),
-        ui.MenuItem(text = "View / edit sources", function = edit_sources),
+        ui.MenuItem(text = "View / edit sources", function = view_sources),
         ui.MenuItem(text = "Cleanup files", function = clean_files),
-        ui.MenuItem(text = "Help", function = help_funct),
         ui.MenuItem(text = "Exit", function = exit),
     ]
 
 def sources_menuitems():
-    global sources
-
-    sources = sh.read_sources_file()
     menuitems = list()
-    for key in sources.keys():
-        url = sources[key]
+    for key in get_sources().keys():
+        url = get_sources()[key]
         menutext = ""
         if key.startswith(sh.BL_PRFX):
             menutext += "Blacklist"
         if key.startswith(sh.WL_PRFX):
             menutext += "Whitelist"
         menutext += ": " + url
+        item = ui.MenuItem(text = menutext, function = edit_source)
+        menuitems.append(item)
+
     return menuitems
 
-def profile_menuitems():
-    global profiles
-    
-    profiles = ph.load_profiles()
+def profile_menuitems(func):
     menuitems = list()
-    for name in profiles.keys():
-        new_menuitem = ui.MenuItem(text = name, function = set_profile)
+    for name in get_profiles().keys():
+        new_menuitem = ui.MenuItem(text = name, function = func)
         menuitems.append(new_menuitem)
     return menuitems
 
 def set_profile(instance):
-    global profile_selected, main_header
+    global profile_selected
 
     # TODO: set 'selected profile' up in header somehow (UI takes header arguments??)
-    profile_selected = list(profiles.keys())[instance.get_returned_index()]
+    profile_selected = get_profile_at(instance.get_returned_index())["NAME"]
+    instance.goto_previous()
+
+def delete_profile(instance):
+    selected_name = get_profile_at(instance.get_returned_index())["NAME"]
+    del profiles[selected_name]
+    os.remove("profiles/" + selected_name + ".profile")
     instance.goto_previous()
 
 ### MenuItem functions
@@ -73,37 +76,93 @@ def compile_blocklist(instance):
     quit()
 
 def view_profiles(instance):
-    instance.set_menu_items(profile_menuitems())
+    instance.set_menu_items(profile_menuitems(set_profile))
     instance.set_header(profile_header)
     instance.run_loop()
 
 def edit_profiles(instance):
+    menuitems = profile_menuitems(edit_profile)
+    createprofile_item = ui.MenuItem(text = "[ Create new profile ]", function = create_profile)
+    menuitems.append(createprofile_item)
+
+    instance.set_menu_items(menuitems)
+    instance.set_header(profile_editor_header)
+    instance.run_loop()
+
+def create_profile(instance):
+    # Create new ProfileEditorInstance and pass control to it
+    profile = dict()
+    profile["NAME"] = ""
+    profile["SOURCES"] = list()
+    editor = ui.ProfileEditorInstance(curses_screen = instance.screen)
+    editor.set_profile(profile)
+    editor.set_sources(get_sources())
+    profile = editor.run_loop()
+    
+    if profile["NAME"] != "" and len(profile["SOURCES"]) > 0:
+        get_profiles()[profile["NAME"]] = profile
+        ph.write_profiles(profiles)
+    
+    # Pass control back to MenuInstance
+    instance.goto_previous()
+
+def edit_profile(instance):
+    # Same sort of issue with mess code here as in 'create_profile'
+    editor = ui.ProfileEditorInstance(curses_screen = instance.screen)
+    index = instance.get_returned_index()
+    editor.set_profile(get_profile_at(index))
+    editor.set_sources(get_sources())
+    result = editor.run_loop()
+    
+    if result["NAME"] != "" and len(result["SOURCES"]) > 0:
+        profiles[index] = result
+        ph.write_profiles(profiles)
+    
+    instance.goto_previous()
+
+def view_sources(instance):
+    instance.set_menu_items(sources_menuitems())
+    instance.set_header(sources_header)
+    instance.run_loop()
+
+def edit_source(instance):
+    # Same sort of issue with mess code here as in 'create_profile'
     pass
 
-def edit_sources(instance):
-    pass
+def clean_files(instance):
+    instance.close()
 
-def clean_files(scrn):
-    pass
+    print("Cleaning up files.")
+    print("Removing /backups directory...")
+    try:
+        files = os.listdir("backups")
+        for f in files:
+            os.remove("backups/" + f)
+    except OSError:
+        print("FAILED DELETING FILES IN /backups")
+    try:
+        os.rmdir("backups")
+    except OSError:
+        print("FAILED DELETING DIRECTORY /backups")
 
-def help_funct(scrn):
-    pass
+    print("Removing /compiled directory...")
+    try:
+        files = os.listdir("compiled")
+        for f in files:
+            os.remove("compiled/" + f)
+    except OSError:
+        print("FAILED DELETING FILES IN /compiled")
+    try:
+        os.rmdir("compiled")
+    except OSError:
+        print("FAILED DELETING DIRECTORY /compiled")
+
+    print("Finished Cleaning!")
+    quit()
 
 def exit(instance):
     instance.close()
     quit()
-
-def name_editor(scrn):
-    pass
-
-def source_selector(scrn):
-    pass
-
-def profile_creator(scrn):
-    pass
-
-def profile_editor(scrn):
-    pass
 
 ### Data Handling
 def get_blacklist_sources(profile):
@@ -126,6 +185,22 @@ def get_whitelist_sources(profile):
             wl.append(source)
     return wl
 
+def get_sources():
+    global sources
+    if not sources:
+        sources = sh.read_sources_file()
+    return sources
+
+def get_profiles():
+    global profiles
+    if not profiles:
+        profiles = ph.load_profiles()
+    return profiles
+
+def get_profile_at(i):
+    keys = list(get_profiles().keys())
+    return profiles[keys[i]]
+
 ### Menu functions
 def loop_main(instance):
     instance.set_header(main_header)
@@ -142,5 +217,5 @@ def run():
         if instance:
             instance.close()
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     run()
--- ui.py
+++ ui.py
@@ -1,6 +1,8 @@
 import curses
 
-# TODO: clean this code up and separate out to release as separate library / module
+profile_header = "Profile Editor Form"
+profile_name_field = "Name: "
+profile_sources_field = "Sources: "
 
 class MenuItem:
     """
@@ -27,17 +29,17 @@ class MenuInstance:
         
         self.header_message = None
         self.previous_header_message = None # TODO: turn this into a stack to handle >2 nested menus
-        self.next_header_message = None
+        self._next_header_message = None
         
         self.menu_items = None
         self.previous_menu_items = None # TODO: turn this into a stack to handle >2 nested menus
-        self.next_menu_items = None
+        self._next_menu_items = None
         
-        self.item_selected = 0
-        self.previous_selected = -1
-        self.return_item_index = -1
+        self._item_selected = 0
+        self._previous_selected = -1
+        self._return_item_index = -1
         
-        self.next_function = None
+        self._next_function = None
         
         self.debug = debug
     
@@ -72,7 +74,7 @@ class MenuInstance:
         
         if self.menu_items:
             self.previous_menu_items = self.menu_items
-        self.next_menu_items = menu_items
+        self._next_menu_items = menu_items
     
     def set_header(self, header_message = ""):
         """
@@ -82,15 +84,15 @@ class MenuInstance:
         
         if self.header_message:
             self.previous_header_message = self.header_message
-        self.next_header_message = header_message
+        self._next_header_message = header_message
     
     def get_returned_index(self):
         """
         Returns + resets the selected item and caches it under previous_selected
         """
         
-        temp = self.return_item_index
-        self.return_item_index = -1
+        temp = self._return_item_index
+        self._return_item_index = -1
         return temp
 
     def goto_previous(self):
@@ -100,21 +102,20 @@ class MenuInstance:
 
         # TODO: find more elegant way of doing this
         # Sets up next menu items to use
-        self.next_menu_items = self.previous_menu_items
+        self._next_menu_items = self.previous_menu_items
         self.previous_menu_items = None
 
         # Sets up next header to use
-        self.next_header_message = self.previous_header_message
+        self._next_header_message = self.previous_header_message
         self.previous_header_message = None
 
         # Sets up previously selected item index + resets previously selected
-        self.item_selected = self.previous_selected
-        self.previous_selected = -1
+        self._item_selected = self._previous_selected
+        self._previous_selected = -1
 
         # TODO: clear next
         # Sets to go back to run_loop
         self.run_loop()
-
     
     def run_loop(self):
         """
@@ -122,12 +123,12 @@ class MenuInstance:
         """
         
         # Grab next set of menu_items to use
-        self.menu_items = self.next_menu_items
-        self.next_menu_items = None
+        self.menu_items = self._next_menu_items
+        self._next_menu_items = None
         
         # Grab header message to use
-        self.header_message = self.next_header_message
-        self.next_header_message = None
+        self.header_message = self._next_header_message
+        self._next_header_message = None
         
         # Initial draw
         self.screen.clear()
@@ -135,7 +136,6 @@ class MenuInstance:
         self._draw_menu()
         if self.debug: self._draw_debug()
         self.screen.refresh()
-        
         # Main loop
         while True:
             result = self._handle_input()
@@ -149,10 +149,10 @@ class MenuInstance:
             
         # Call function set by input handler
         # TODO: improve, hacky way of handling this...
-        if self.next_function == self.run_loop:
+        if self._next_function == self.run_loop:
             self.run_loop()
         else:
-            self.next_function(self)
+            self._next_function(self)
     
     def _handle_input(self):
         """
@@ -173,46 +173,46 @@ class MenuInstance:
                 return False
 
             # Sets up next menu items to use
-            self.next_menu_items = self.previous_menu_items
+            self._next_menu_items = self.previous_menu_items
             self.previous_menu_items = None
 
             # Sets up next header to use
-            self.next_header_message = self.previous_header_message
+            self._next_header_message = self.previous_header_message
             self.previous_header_message = None
 
             # Sets up previously selected item index + resets previously selected
-            self.item_selected = self.previous_selected
-            self.previous_selected = -1
+            self._item_selected = self._previous_selected
+            self._previous_selected = -1
 
             # Sets to go back to run_loop
-            self.next_function = self.run_loop
+            self._next_function = self.run_loop
             return True
         elif key == "KEY_RIGHT":
             # Sets up next function to call
-            self.next_function = self.menu_items[self.item_selected].function
+            self._next_function = self.menu_items[self._item_selected].function
             
             # saves currently selected item value for return in case needed
-            self.return_item_index = self.item_selected
+            self._return_item_index = self._item_selected
             
             # resets item selected index for next screen
-            self.previous_selected = self.item_selected
-            self.item_selected = 0
+            self._previous_selected = self._item_selected
+            self._item_selected = 0
             
             return True
         
         return False
     
     def _inc_selected_item(self):
-        if self.item_selected == len(self.menu_items) - 1:
-            self.item_selected = 0
+        if self._item_selected == len(self.menu_items) - 1:
+            self._item_selected = 0
         else:
-            self.item_selected += 1
+            self._item_selected += 1
     
     def _dec_selected_item(self):
-        if self.item_selected == 0:
-            self.item_selected = len(self.menu_items) - 1
+        if self._item_selected == 0:
+            self._item_selected = len(self.menu_items) - 1
         else:
-            self.item_selected -= 1
+            self._item_selected -= 1
     
     def _draw_header(self):
         for line in self.header_message.split("\n"):
@@ -224,7 +224,7 @@ class MenuInstance:
         index = 0
         for item in self.menu_items:
             text = "> " + item.text + "\n"
-            if index == self.item_selected:
+            if index == self._item_selected:
                 self.screen.addstr(text, curses.A_REVERSE)
             else:
                 self.screen.addstr(text)
@@ -235,4 +235,156 @@ class MenuInstance:
             # Keep 3 blank lines after menu
             self.screen.addstr("\n")
         self.screen.addstr("----------------------------------\n")
-        self.screen.addstr("item_selected = %d\nprevious_selected = %d\nreturn_item_index = %d" % (self.item_selected, self.previous_selected, self.return_item_index))
+        self.screen.addstr("item_selected = %d\nprevious_selected = %d\nreturn_item_index = %d" % (self._item_selected, self._previous_selected, self._return_item_index))
+        
+class ProfileEditorInstance:
+    def __init__(self, curses_screen = None, debug = False):
+        self.screen = curses_screen
+
+        self._profile = None
+        self._sources = None
+
+        self._item_selected = 0
+        self._input_enabled = True
+        self._unsaved_name = None
+
+        self.debug = debug
+    
+    def set_profile(self, profile = None):
+        self._profile = profile
+    
+    def set_sources(self, sources = list()):
+        self._sources = sources
+    
+    def run_loop(self):
+        # Initial draw
+        self.screen.clear()
+        self._draw_header()
+        self._draw_profile_form()
+        if self.debug: self._draw_debug()
+        self.screen.refresh()
+        
+        while True:
+            result = self._handle_input()
+            if result: break
+            
+            self.screen.clear()
+            self._draw_header()
+            self._draw_profile_form()
+            if self.debug: self._draw_debug()
+            self.screen.refresh()
+        
+        return self._profile
+    
+    def _draw_header(self):
+        for line in profile_header.split("\n"):
+            text = ">>>\t" + line + "\n"
+            self.screen.addstr(text, curses.A_BOLD)
+        self.screen.addstr("\n")
+    
+    def _draw_profile_form(self):
+        self.screen.addstr("> " + profile_name_field + "\n")
+        name_text = "--| " + self._profile["NAME"]
+        
+        if self._item_selected == 0:
+            self.screen.addstr(name_text, curses.A_REVERSE)
+            self._input_enabled = True # time to enable input! This flag needs to be set here so _handle_input() can act appropriately on next loop
+        else:
+            self.screen.addstr(name_text)
+
+        self.screen.addstr("\n")
+        self.screen.addstr("> " + profile_sources_field + "\n")
+        index = 1
+        for key in self._sources.keys():
+            text = " ["
+            if key in self._profile["SOURCES"]:
+                text += "x"
+            else:
+                text += " "
+            text += "] "
+            if key.startswith("b_"):
+                text += "Blacklist: "
+            if key.startswith("w_"):
+                text += "Whitelist: "
+            text += self._sources[key] + "\n"        
+            if self._item_selected == index:
+                self.screen.addstr(text, curses.A_REVERSE)
+            else:
+                self.screen.addstr(text)
+            index += 1
+            
+    def _draw_debug(self):
+        for i in range(3):
+            # Keep 3 blank lines after menu
+            self.screen.addstr("\n")
+        self.screen.addstr("----------------------------------\n")
+        self.screen.addstr("item_selected = %d\nprevious_selected = %d\nreturn_item_index = %d" % (self._item_selected, self._previous_selected, self._return_item_index))
+
+    def _inc_selected_item(self):
+        if self._item_selected == len(self._sources):
+            self._item_selected = 0
+        else:
+            self._item_selected += 1
+    
+    def _dec_selected_item(self):
+        if self._item_selected == 0:
+            self._item_selected = len(self._sources)
+        else:
+            self._item_selected -= 1
+
+    def _handle_input(self):
+        """
+        Handle input from run loop. Stalls until getkey() returns a value.
+        Only returns true if 'back' button pressed and no chars to delete
+        """
+        
+        key = self.screen.getkey()
+  
+        if key == "KEY_UP":
+            # Move up the list
+            self._dec_selected_item()
+            if self._input_enabled: self._input_enabled = False
+        elif key == "KEY_DOWN":
+            # Move down the list
+            self._inc_selected_item()
+            if self._input_enabled: self._input_enabled = False
+        elif key == "KEY_LEFT":
+            # Need to end loop and go back to previous screen (pass control back over to MenuInstance)
+            if self._item_selected == 0:
+                length = len(self._profile["NAME"])
+                if length > 1:
+                    self._profile["NAME"] = self._profile["NAME"][:length - 1]
+                    return False
+                elif length == 1:
+                    self._profile["NAME"] = ""
+                    return False
+            return True
+        elif key == "KEY_RIGHT":
+            if self._item_selected == 0: # do nothing on name editing line
+                return False
+            # If a source item is highlighted, then allow enabling / disabling of it
+            y_line = len(profile_header.split("\n")) + 3 + self._item_selected
+            source = list(self._sources.keys())[self._item_selected - 1] # -1 since sources start on 1st line, not 0th
+            if source in self._profile["SOURCES"]:
+                self._profile["SOURCES"].remove(source)
+                text = " "
+            else:
+                self._profile["SOURCES"].append(source)
+                text = "x"
+            self.screen.addnstr(y_line, 2, text, 1)
+            self.screen.redrawln(y_line, 1)
+        else:
+            if self._item_selected != 0:
+                return False
+            # Time to start accepting input!
+            # (need to draw the text box on the correct line)
+            y_line = len(profile_header.split("\n")) + 2
+            self.screen.redrawln(y_line, 1)
+            
+            if key.isalpha() or key.isdigit() or key in "+-=_":
+                self._profile["NAME"] += key
+            
+            self.screen.addnstr(y_line, 3, self._profile["NAME"] + " ", len(self._profile["NAME"]) + 1) # Add whitespace char at end to ensure we always overwrite previous even if backspaced
+            return self._handle_input()
+        
+        return False
